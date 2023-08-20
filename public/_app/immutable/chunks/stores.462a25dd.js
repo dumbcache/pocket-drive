3f9a4bb082e334c5c685f5be0a26ce06bc818e1d
dumbcache <dumbcache@gmail.com>
2023-08-21 01:21:44 +0530

    added firebase

---
+++ public/_app/immutable/chunks/stores.462a25dd.js
@@ -0,0 +1,1 @@
+import"./index.8ed2abbf.js";import{s as r}from"./singletons.98e22c66.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},o={subscribe(s){return e().navigating.subscribe(s)}};export{o as n,a as p};
